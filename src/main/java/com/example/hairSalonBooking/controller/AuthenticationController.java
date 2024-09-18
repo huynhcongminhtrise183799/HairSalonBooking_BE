4f766e342bb9eb4022fa59a06660373b5f62b23a
huynhcongminhtrise183799 <trihcmse183799@fpt.edu.vn>
2024-09-18 21:32:53 +0700

    test

--- src/main/java/com/example/hairSalonBooking/controller/AuthenticationController.java
+++ src/main/java/com/example/hairSalonBooking/controller/AuthenticationController.java
@@ -1,7 +1,9 @@
 package com.example.hairSalonBooking.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("")
 public class AuthenticationController {
 }
