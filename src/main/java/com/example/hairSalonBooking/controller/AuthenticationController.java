b64deb5703df6003e0be55a6469e48646925f537
ThanhLong-FE <occurmikey@gmail.com>
2024-09-18 22:07:07 +0700

    update account

---
+++ src/main/java/com/example/hairSalonBooking/controller/AuthenticationController.java
@@ -0,0 +1,10 @@
+package com.example.hairSalonBooking.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@RestController
+@RequestMapping("")
+public class AuthenticationController {
+}
