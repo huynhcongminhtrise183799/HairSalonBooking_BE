db29559f78156a427d55a868708e7896142d1c29
huynhcongminhtrise183799 <trihcmse183799@fpt.edu.vn>
2024-09-20 10:36:15 +0700

    update customer profile

---
+++ src/main/java/com/example/hairSalonBooking/service/CustomerService.java
@@ -0,0 +1,32 @@
+package com.example.hairSalonBooking.service;
+
+import com.example.hairSalonBooking.entity.Account;
+import com.example.hairSalonBooking.exception.NotFoundException;
+import com.example.hairSalonBooking.model.request.UpdateCustomerRequest;
+import com.example.hairSalonBooking.model.response.UpdateCustomerResponse;
+import com.example.hairSalonBooking.repository.CustomerRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomerService {
+
+    @Autowired
+    CustomerRepository customerRepository;
+    //@Autowired
+    //ModelMapper modelMapper;
+    public UpdateCustomerRequest updateCustomer(UpdateCustomerRequest request, long AccountId){
+        Account account = customerRepository.findAccountByAccountid(AccountId);
+        if(account == null){
+            throw new NotFoundException("Account not found");
+        }
+        account.setFullname(request.getFullName());
+        account.setPhone(request.getPhone());
+        account.setDob(request.getDob());
+        account.setEmail(request.getEmail());
+        customerRepository.save(account);
+        return request;
+
+    }
+}
