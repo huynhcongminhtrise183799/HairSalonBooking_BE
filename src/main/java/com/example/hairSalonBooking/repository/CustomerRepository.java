bddd175d164e3d8376133399b2ec9f8a06ad42be
huynhcongminhtrise183799 <trihcmse183799@fpt.edu.vn>
2024-09-26 23:29:25 +0700

    update Entity

--- src/main/java/com/example/hairSalonBooking/repository/CustomerRepository.java
+++ src/main/java/com/example/hairSalonBooking/repository/CustomerRepository.java
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CustomerRepository extends JpaRepository<Account,Long> {
     Account findAccountByAccountid(long AccountID);
+    Account findAccountByUsername(String username);
 }
