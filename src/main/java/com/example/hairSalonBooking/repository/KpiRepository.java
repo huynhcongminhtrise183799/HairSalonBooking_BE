b255808805ee4e5f0736e6abed6afcbf2bc06d0b
thành-long <occurmikey@gmail.com>
2024-10-30 19:10:10 +0700

    Update KpiRepository.java

--- src/main/java/com/example/hairSalonBooking/repository/KpiRepository.java
+++ src/main/java/com/example/hairSalonBooking/repository/KpiRepository.java
@@ -8,4 +8,10 @@ import org.springframework.data.jpa.repository.Query;
 public interface KpiRepository extends JpaRepository<Kpi, Long> {
     @Query("SELECT k FROM Kpi k WHERE k.level = :level AND k.yearAndMonth = :yearAndMonth")
     Kpi findByLevelAndYearAndMonth(Level level, String yearAndMonth);
+
+    @Query("SELECT k.bonusPercent FROM Kpi k " +
+            "WHERE k.level.levelid = :levelId " +
+            "AND :totalRevenue " +
+            "BETWEEN k.revenueFrom AND k.revenueTo")
+    Optional<Double> findBonusPercentageByRevenueAndLevel(@Param("levelId") long levelId, @Param("totalRevenue") double totalRevenue);
 }
