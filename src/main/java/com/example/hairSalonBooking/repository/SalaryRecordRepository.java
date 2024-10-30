bbd5888ab759e884bcbd195d9025b533b124fd40
thành-long <occurmikey@gmail.com>
2024-10-30 19:15:08 +0700

    Update SalaryRecordRepository.java

--- src/main/java/com/example/hairSalonBooking/repository/SalaryRecordRepository.java
+++ src/main/java/com/example/hairSalonBooking/repository/SalaryRecordRepository.java
@@ -4,4 +4,10 @@ import com.example.hairSalonBooking.entity.SalaryRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SalaryRecordRepository extends JpaRepository<SalaryRecord, Long> {
+  @Query("SELECT sr FROM SalaryRecord sr " +
+        "JOIN sr.account a " +
+        "WHERE a.salonBranch.salonId = :salonId " +
+        "AND sr.monthAndYear = :monthAndYear")
+List<SalaryRecord> findSalaryRecordsBySalonIdAndYearMonth(@Param("salonId") Long salonId,
+                                                            @Param("monthAndYear") String monthAndYear);
 }
